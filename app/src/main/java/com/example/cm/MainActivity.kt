1a7024b4356d43857ed34d0a5ef9c92d93169d59
Culverin21 <48128893+Culverin21@users.noreply.github.com>
2021-03-10 15:46:10 +0000

    Criação da atividade para as notas pessoais e também do botão que dá acesso a essa atividade

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.cm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.buttonNotas)
+        button.setOnClickListener{
+            val intent = Intent(this, NotasActivity::class.java)
+            startActivity(intent)
+        }
     }
 
- fun tentativa(){
-     //asdasd
- }
 
-    fun testes(){
-    //asddasdas
-    }
 }
---
+++ app/src/main/java/com/example/cm/NotasActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notas)
+
+    }
+}
